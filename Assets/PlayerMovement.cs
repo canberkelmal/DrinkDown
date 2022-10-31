fb78d265ea3173fa978740d06075a79cc1b9b700
Canberk Elmal <elmalcanberk@gmail.com>
2022-10-31 21:06:06 +0300

    FirstPush

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    Rigidbody rb;
+    public float sensivity=100f;
+    public int health;
+    public Joystick joystick;
+    public Text score;
+
+    void Start()
+    {
+        health=100;
+        rb=gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(joystick.Vertical>=0){
+            rb.velocity=new Vector3(joystick.Horizontal*sensivity,
+                                    0,
+                                    joystick.Vertical*sensivity);
+        }
+        else{
+            rb.velocity=new Vector3(joystick.Horizontal*sensivity, 0, 0);
+        }
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other){
+        
+        Debug.Log(other.gameObject.tag);
+
+        if(other.gameObject.tag=="Heal" || other.gameObject.tag=="HealGate"){
+            Debug.Log("from " + health);
+            health+=10;
+            Debug.Log("to " + health);
+            Debug.Log("----------------");
+            score.text="Health: " + health;
+        }
+
+        if(other.gameObject.tag=="Fire" || other.gameObject.tag=="DamageGate"){
+            Debug.Log("from " + health);
+            health-=10;
+            Debug.Log("to " + health);
+            Debug.Log("----------------");
+            score.text="Health: " + health;
+        }
+
+        if(other.gameObject.tag=="Fire" || other.gameObject.tag=="Heal"){
+            Destroy(other.gameObject);
+        }
+
+    }
+}
